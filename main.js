420941e1a0482355831f89e4e504e8d6848b45a2
Ananth Murugan G K <ananthmgk4002@gmail.com>
2023-01-08 14:58:10 +0530

    created Hold shift and check multiple.

---
+++ main.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+  const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+  let lastChecked;
+  function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+      checkboxes.forEach((checkbox) => {
+        console.log(checkbox);
+        if (checkbox === lastChecked || checkbox === this) {
+          inBetween = !inBetween;
+          console.log(inBetween);
+          console.log('Starting to check them in between!');
+        }
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
+    }
+
+    lastChecked = this;
+  }
+
+  checkboxes.forEach((checkbox) =>
+    checkbox.addEventListener('click', handleCheck)
+  );
+});
